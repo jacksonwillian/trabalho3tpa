033aa48d638015205d47f0363bea124e95e0f506
Vinicius <viniciusrocha28@gmail.com>
2019-11-01 18:44:29 -0300

    uva514

---
+++ uva00514/uva00514.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * MAIN514
+ */
+    class M514 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNext()) {
+            int n = scanner.nextInt();
+
+            while (n != 0) {
+
+                int[] in = new int[n];
+
+                for (int i = 0; i < n; i++) {
+                    int x = scanner.nextInt();
+                    if (x != 0) {
+                        in[i] = x;
+                    }
+                    else {
+                        n = 0;
+                        break;
+                    }
+                }
+                Integer k = 1;
+                Integer i = 0;
+        
+                Stack<Integer> stack = new Stack<>();
+        
+                while (k <= n) {
+                    stack.add(k);
+                    k=k+1;
+                    while (stack.peek() < in[i]) {
+                        stack.add(k);
+                        k=k+1;
+                    }
+        
+                    while (stack.peek() == in[i]) {
+                        stack.pop();
+                        i = i+1;
+                        if (stack.size() == 0) {
+                            break;
+                        }
+                    }
+                }
+                if (n == 0){
+                    System.out.println("");
+                }
+                else if (i == n){
+                    System.out.println("Yes");
+                }
+                else{
+                    System.out.println("No");              
+                }
+            }
+        }
+        scanner.close();
+    }
+}
