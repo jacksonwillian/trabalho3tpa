79a727a0f4a537ab31a74bc9a47d7d906f7ffaef
Jackson Willian <jacksonwilliansilvaagostinho@gmail.com>
2019-11-07 17:27:22 -0300

    11926 ainda com bug

--- uva 11926/Main.java
+++ uva 11926/Main.java
@@ -7,90 +7,104 @@ public class Main {
         
         Scanner scanner = new Scanner(System.in);
         
-        BitSet agenda_bit = new BitSet(1000010);
+        BitSet agenda_bit = new BitSet(1000001);
         String [] linha;
 
         String buf =  scanner.nextLine();
         linha = buf.split(" ");
         
-        int quant_unico = Integer.parseInt(linha[0]);
-        int quant_rept  = Integer.parseInt(linha[1]);
+        int quant_unico;
+        int quant_rept;
         
         int inic;
         int end;
         int inter;
        
         
-        boolean no_conflit = true;
+        boolean conflit;
         
-        while (scanner.hasNextLine()){
+        while (scanner.hasNextLine() && !buf.equals("0 0")){
+            
+            conflit = false;
+            
+            quant_unico = Integer.parseInt(linha[0]);
+            quant_rept  = Integer.parseInt(linha[1]);
+            
+            //System.out.println(quant_unico + " " + quant_rept);
+            
+            for(int i = 0; i < quant_unico;i++){
+                
+                buf = scanner.nextLine();
+                linha = buf.split(" ");
+                //System.out.println(buf);
+                
+
+                inic    = Integer.parseInt(linha[0]);
+                end     = Integer.parseInt(linha[1]);
+
+                if(!conflit){
+                    for(int j = inic; j < end ; j++){
+                        if(agenda_bit.get(j)){
+                            conflit = true;
+                            break;
+                        }
+
+                            agenda_bit.set(j,true);
+                    }
+                }
+                
+            }
+            
+            for(int i = 0; i < quant_rept;i++){
+                
+                buf = scanner.nextLine();
+                linha = buf.split(" ");
+                
+                
+                    
+                inic    = Integer.parseInt(linha[0]);
+                end     = Integer.parseInt(linha[1]);
+                inter   = Integer.parseInt(linha[2]);
+
+                if(!conflit){
+
+                    while(inic < agenda_bit.size() && end < agenda_bit.size()){
+
+                        for(int j = inic; j < end ;j++){
+                            if(agenda_bit.get(j)){
+                                conflit = true;
+                                break;
+
+                            }
+                            agenda_bit.set(j,true);
+
+                        }
+                        inic += inter;
+                        end += inter;
+                        
+                        if(end > agenda_bit.size()-1){
+                            end = agenda_bit.size()-1;
+                        }
+                    }
+                }
+            }
+
+            
+            
+            if(conflit){
+                System.out.println("CONFLICT");
+            }else{
+                System.out.println("NO CONFLICT");
+            }
             
             buf = scanner.nextLine();
             linha = buf.split(" ");
             
-            
-            if(quant_unico > 0 && no_conflit){
-                    
-                    
-                inic    = Integer.parseInt(linha[0]);
-                end     = Integer.parseInt(linha[1]);
-              
-              
-                if(agenda_bit.get(inic,end).cardinality()>0){
-                    
-                    no_conflit = false;
-                    
-                }else{
-  
-                    agenda_bit.set(inic, end);
-                }
-                    
-                quant_unico--;
-                
-            }else if(quant_rept > 0 && no_conflit) {
-                
-                inic    = Integer.parseInt(linha[0]);
-                end     = Integer.parseInt(linha[1]);
-                inter   = Integer.parseInt(linha[2]);
-                
-                for(int i = 0; i < agenda_bit.size();i +=inter){
-                    
-                    if(agenda_bit.get(inic+i,end+i).cardinality()>0){
-                    
-                        no_conflit = false;
+            agenda_bit.clear();
 
-                    }else{
-                        
-                        agenda_bit.set(inic + i, end + i);
-                    }
-                
-                }
-                
-                quant_rept--;
             
-            }else{
-                
-                if(quant_unico == 0 && quant_rept == 0 && !buf.equals("")){
-                        
-                        quant_unico = Integer.parseInt(linha[0]);
-                        quant_rept  = Integer.parseInt(linha[1]);
-                        
-                        if(no_conflit){
-                            System.out.println("NO CONFLICT");
-
-                        }else{
-                            System.out.println("CONFLICT");
-                            no_conflit = true;           
-                        }
-                        if(!buf.equals("")){
-                            quant_unico = Integer.parseInt(linha[0]);
-                            quant_rept  = Integer.parseInt(linha[1]);
-                            agenda_bit.clear();
-                        }
-                }
-            }
         }
-
+        // System.out.println();
         scanner.close();
     }
 }
