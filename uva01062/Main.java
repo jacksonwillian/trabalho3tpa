8718b89085d955a31048f8f20d6a229105d7504c
ViniciusFR <43283952+Viniciusfr123@users.noreply.github.com>
2019-11-09 23:09:47 -0300

    Update Main.java

--- uva01062/Main.java
+++ uva01062/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 import java.util.LinkedList;
 
 /**
- * Uva01062
+ * Vinicius Freitas Rocha
+    01062
  */
 public class Main {
     public static void main(String[] args) {
