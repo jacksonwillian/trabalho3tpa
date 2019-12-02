bf57d5f0698f8950064bb975e59cd4369a1a7e4b
Jackson Willian <43284155+jacksonwillian@users.noreply.github.com>
2019-12-02 09:36:34 -0300

    Update uva11340.py

--- uva11340/uva11340.py
+++ uva11340/uva11340.py
@@ -1,37 +1,97 @@
-# 11340 Newspaper
-# Autores: Jackson Willian
 
 import sys 
 
-def main():
 
-	quantTeste = int(sys.stdin.readline())
-	for i in range (quantTeste):
-		quantLinha = int(sys.stdin.readline())
-		listaPreco = []
-		for j in range (quantLinha):
-			linha =  sys.stdin.readline()
-			listaPreco.append( [  linha.split()[0], int( linha.split()[1] ) ] )
+# Autores: Jackson Willian
+# Problema: UVa 11100 –  The Trip, 2007 
 
-		valor = 0
-		linha = ""
-		quantLinha = int(sys.stdin.readline())
-		for j in range (quantLinha):
 
-			linha +=  sys.stdin.readline()
+def addBolsa(setBolsa, bolsa):
 
-		for k in range ( len(listaPreco) ):
-			frequencia = linha.count(listaPreco[k][0]) 
-			valor += frequencia * listaPreco[k][1] if frequencia > 0 else 0
-		print("%.2f$" %(valor/100))
+	menor = -1
+	indice = -1
+	for i in range( len(setBolsa) ):
+		if (menor == -1) or ( menor > len(setBolsa[i]) ):
+			menor = len(setBolsa[i])
+			indice = i
 
+	if (indice != -1) and (bolsa not in setBolsa[indice]):
+
+		setBolsa[indice].append(bolsa)
+		setBolsa[indice].sort()
+		
+	return None
+
+		
+
+
+def quantListas( lstBolsas ):
+	conjunto =  set(lstBolsas)
+	maior = 0
+	for i in conjunto:
+		freq = lstBolsas.count(i)
+		if ( maior <  freq):
+			maior = freq
+	return maior
+			
+	
+	
+	
+	
+	
+	
 	
 
+	
+	
+	
+	
+
+
+def main():
+	
+	quantTeste = int( sys.stdin.readline() )
+	
+	while ( quantTeste != 0 ):
+		
+		lstBolsas = []
+	
+		texto = sys.stdin.readline()
+		texto = texto.strip()
+			
+		for bolsa in texto.split(" "):
+			if (bolsa != " " and bolsa != ""):
+				lstBolsas.append( int(bolsa) )
+
+		lstBolsas.sort()
+
+		tamanho = quantListas( lstBolsas )
+		
+		setBolsa = []
+		for i in range (tamanho):
+
+			setBolsa.append([])
+
+	
+		
+
+		for bolsa in lstBolsas:
+
+			addBolsa(setBolsa, bolsa)
+
+		print(len(setBolsa))
+		for sets in setBolsa:
+			linha = ""
+			for bolsa in sets:
+				linha += str(bolsa) + " "
+			print(linha.strip())
+
+		quantTeste = int( sys.stdin.readline() )
+
+		if(quantTeste != 0):
+			print()
+		
 	return 0
-
+	
 main()
-
-
-
-
 
